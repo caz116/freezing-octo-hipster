d9daaf577a3b65e7f738fb92de08c9c30d0fc437
アカネコ <>
2012-08-04 15:32:51 +0100

    improving the fighter scrit
    added spedomiter also iproved moddle added lighing and changed controles
    form e/q to w/s

--- Assets/Elipsis_Assets/Scripts/FighterMovement.js
+++ Assets/Elipsis_Assets/Scripts/FighterMovement.js
@@ -6,15 +6,19 @@ function Start () {
 }
 
 function Update () {
- if(Input.GetKeyDown("e")) {
+ if(Input.GetKeyDown("w")) {
  	if(spd < Mspd) {
  		spd+=0.1;
  	}
  }
- if(Input.GetKeyDown("q")) {
+ if(Input.GetKeyDown("s")) {
  	if(spd > 0.0) {
  		spd-=0.1;
  	}
  }
  transform.Translate(0,0,spd, Space.Self);
 }
+var la = spd;
+function OnGUI (){
+		 GUI.Label (Rect (10, 10, 100, 20), "Speed : " + spd);
+	}
